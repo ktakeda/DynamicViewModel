5be4d35718182ce465ea5b36d95d57eae8e65cca
k.takeda <k.takeda2007@gmail.com>
2013-08-23 00:57:56 +0900

    first commit

---
+++ DynamicViewModel/DynamicViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Dynamic;
+using System.Reflection;
+using System.ComponentModel;
+
+//モデル - ビュー - ビューモデル (MVVM: Model-View-ViewModel) の問題点とその解決策
+//http://msdn.microsoft.com/ja-jp/magazine/ff798279.aspx
+
+namespace DynamicViewModel
+{
+    public class DynamicViewModel<T> : DynamicObject, INotifyPropertyChanged
+    {
+        /// <summary>バインドさせたいクラス</summary>
+        private T TargetData { get; set; }
+
+        /// <summary> INotifyPropertyChangedの実装 </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary> コンストラクタ </summary>
+        public DynamicViewModel(T targetData)
+        {
+            TargetData = targetData;
+        }
+
+        /// <summary> Getter </summary>
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            //TargetDataの同名のプロパティ
+            var prop = typeof(T).GetProperty(binder.Name);
+
+            //TargetDataに同じ名前のプロパティがある場合には
+            if (prop != null)
+            {
+                //TargetDataのプロパティの値を返す
+                result = prop.GetValue(TargetData, null);
+                return true;
+            }
+            else
+            {
+                result = null;
+                return false;
+            }
+        }
+
+        /// <summary> Setter </summary>
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            //TargetDataの同名のプロパティ
+            var prop = typeof(T).GetProperty(binder.Name);
+
+            //TargetDataに同じ名前のプロパティがある場合には
+            if (prop != null)
+            {
+                //TargetDataのプロパティに値を設定して、値が設定されたことを通知
+                prop.SetValue(TargetData, value, null);
+                PropertyChanged(this, new PropertyChangedEventArgs(binder.Name));
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using DynamicViewModel;
+
+using System.Dynamic;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //実際に編集したいデータ
+        public Person Target;
+
+        //画面にバインドする用のデータ（画面上でのデータ編集をPropertyChangedイベントで補足できる）
+        public DynamicViewModel<Person> BindingTarget;
+
+        public MainWindow()
+        {
+            //編集したいオブジェクト
+            Target = new Person { Name = "のっち" };
+
+            //画面に見せる用のオブジェクト
+            BindingTarget = new DynamicViewModel<Person>(Target);
+
+            //画面でデータに変更があったときに呼ばれるイベントハンドラ
+            BindingTarget.PropertyChanged += new PropertyChangedEventHandler(BindingTarget_PropertyChanged);
+
+            //お約束
+            InitializeComponent();
+            DataContext = BindingTarget;
+        }
+
+        void BindingTarget_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            //画面でデータが変更されるとデバッグ出力に編集後のNameが表示される。
+            Debug.WriteLine("Name = " + Target.Name);
+        }
+    }
+}
